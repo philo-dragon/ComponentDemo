8e362b24a0bf799a206c3cc4fc40de7d5e95e611
philo <panfeilong2012@163.com>
2018-02-01 22:06:23 +0800

    登录添加输入框

--- app/src/main/java/com/pfl/component/ui/SplashActivity.java
+++ app/src/main/java/com/pfl/component/ui/SplashActivity.java
@@ -2,11 +2,13 @@ package com.pfl.component.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.animation.Animation;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
@@ -43,5 +45,20 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return false;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
